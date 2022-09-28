de31f1270a700d3c00af3fba9afc91ae20cac9b8
tlee152 <tlee152@ford.com>
2022-09-28 11:25:06 -0400

    code refactoring

--- homeworks/homework_3/guessing_game/guessing_game.cpp
+++ homeworks/homework_3/guessing_game/guessing_game.cpp
@@ -1,32 +1,42 @@
 #include <iostream>
 #include <random>
 
-int main() {
-  int long smallest_number{}, largest_number{}, user_guess{}, guess_count{1};
-  std::random_device random_device;
-  std::mt19937 random_engine{random_device()};
-  // print welcome message
+void welcome_message() {
   std::cout << "Welcome to the GUESSING GAME!" << std::endl;
   std::cout << "I will generate a number and you will guess it!" << std::endl;
+}
 
-  // get a smallest number from user
+int long random_number_generation() {
+  int long smallest_number{}, largest_number{};
+  std::random_device random_device;
+  std::mt19937 random_engine{random_device()};
+
   std::cout << "Please provide the smallest number: " << std::endl;
   std::cin >> smallest_number;
 
-  // get a largest number from user
   std::cout << "Please provide the largest number: " << std::endl;
   std::cin >> largest_number;
 
-  // generage a random number between the smallest number and largest number
   std::uniform_int_distribution<int long> distribution{smallest_number,
                                                        largest_number};
-  auto answer = distribution(random_engine);
   std::cout << "I've generated a number. Try to guess it!" << std::endl;
+  return distribution(random_engine);
+}
 
-  // start guessing game with user
+int long guess_from_user() {
+  int long user_guess{};
+  std::cout << "Please provide the next guess: ";
+  std::cin >> user_guess;
+  return user_guess;
+}
+
+void guessing_game() {
+  int guess_count{1};
+  welcome_message();
+  int long answer = random_number_generation();
+  auto user_guess = guess_from_user();
+
   while (true) {
-    std::cout << "Please provide the next guess: ";
-    std::cin >> user_guess;
     if (user_guess < answer) {
       std::cout << "Your number is too small. Try again!" << std::endl;
       guess_count += 1;
@@ -36,11 +46,15 @@ int main() {
       guess_count += 1;
       continue;
     }
-    // end game as guess is correct
+
     std::cout << "You've done it! You guessed the number " << answer << " in "
               << guess_count << " guesses!" << std::endl;
     break;
   }
+}
+
+int main() {
+  guessing_game();
 
   return 0;
 }
